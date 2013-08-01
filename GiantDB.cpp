0a6056da1ea10381ce980c8c34a371fceb91956b
AIRIA <chaibingcheng0305@163.com>
2013-08-01 11:01:02 +0800

    封装了SQLite3的api 包括打开数据库 执行DDL语句和查询语句 并反悔查询记录

---
+++ GiantDB.cpp
@@ -0,0 +1,56 @@
+﻿#include "GiantDB.h"
+
+GiantDB *GiantDB::s_pInstance = NULL;
+
+GiantDB *GiantDB::instance() {
+    if(s_pInstance==NULL) {
+        s_pInstance = new GiantDB();
+    }
+    return s_pInstance;
+}
+
+void GiantDB::GiantLog(const char *info) {
+    if(debug==false) {
+        return;
+    }
+#ifdef CONSOLE
+    cout << "[GiantDB] " << info << endl;
+#endif
+#ifdef CC
+    string log = "[GiantDB] ";
+    CCLog(log+info);
+#endif
+}
+
+void GiantDB::exec_log(int result,const char *successMsg) {
+    if(result==SQLITE_OK) {
+        GiantLog(successMsg);
+    } else {
+        GiantLog(errMsg);
+    }
+}
+/* 打开数据库 */
+void GiantDB::openDB(const char *dbname) {
+    exec_log(sqlite3_open(dbname,&sqlDB),"open database success");
+}
+/* 执行DDL语句 以及更新和删除操作 */
+void GiantDB::exec(const char *sql) {
+    string str = "excute command success:";
+    str = str+sql;
+    exec_log(sqlite3_exec(sqlDB,sql,NULL,NULL,&errMsg),str.c_str());
+}
+
+vector< map<string,string> > GiantDB::query(const char *sql) {
+	res.clear();
+    exec_log(sqlite3_exec(sqlDB,sql,GiantDB::exec_callback,NULL,&errMsg),"data query success");
+    return res;
+}
+
+int GiantDB::exec_callback(void *param,int n_column,char **columnValues,char **columnNames) {
+	map<string,string> record;
+	for(int i=0; i<n_column; i++) {
+		record.insert(make_pair(columnNames[i],columnValues[i]));
+    }
+	s_pInstance->res.push_back(record);
+    return SQLITE_OK;
+}
---
+++ GiantDB.h
@@ -0,0 +1,45 @@
+﻿#ifndef _GIANTDB_H_
+#define _GIANTDB_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+#include "sqlite3.h"
+
+
+#define CONSOLE
+#define GDB Giant
+using namespace std;
+
+class GiantDB {
+public:
+    sqlite3 *sqlDB;
+    int result;
+    char *errMsg;
+    string sql;
+    bool debug;
+	vector< map<string,string> > res;
+public:
+    vector< map<string,string> > query(const char *sql);
+    void exec(const char *sql);
+    void openDB(const char *dbname);
+    static GiantDB *instance();
+    ~GiantDB() {
+        delete s_pInstance;
+    }
+private:
+	int currentIdx;
+    void GiantLog(const char *info);
+    static int exec_callback(void *param,int n_column,char **columnValues, char **columnNames);
+    GiantDB():debug(false),sqlDB(NULL),errMsg(NULL),currentIdx(0) {
+        //禁止实例化
+    };
+    /* 执行完毕后打印注释 在调试模式下使用 */
+    void exec_log(int res,const char *successMsg);
+    static GiantDB *s_pInstance;
+};
+
+
+#endif
